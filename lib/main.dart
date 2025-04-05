c2653b4dfdc39dcf4fce6af6ce5cfc6a02f8f3bf
stanlapru <stanlap@vk.com>
2025-04-05 18:57:51 +0300

    изменил заголовок

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Kurs Flutter',
+      title: 'Курс Flutter Проект Тест',
       initialRoute: "/",
       routes: {
         "/": (context) => const NavigationWrapper(),
